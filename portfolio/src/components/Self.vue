3bae2dabc06376950f36aa76b0694bcc8a533767
Ifeoluwa <iamsamnice@gmail.com>
2024-11-12 15:32:03 -0800

    Me & Transitions Agreed on Terms

--- portfolio/src/components/Self.vue
+++ portfolio/src/components/Self.vue
@@ -6,16 +6,16 @@
     <section class="w-full lg:h-56 flex sm:flex-row md:justify-center gap-4 mb-3 text-white">
             <div class="sm:w-1/3 rounded-xl bg-gradient hidden sm:block relative">
                 <Transition name="purple">
-                    <div class="w-[80%] h-[2px] absolute top-0 rounded-full bg-gradient-to-r from-purple-800 to-[#191919]"></div>
+                    <div class="w-1/4 h-[2px] absolute top-0 rounded-full bg-gradient-to-l from-purple-800 to-[#191919] purple-enter-active opacity-0"></div>
                 </Transition>
                 <Transition name="blue">
-                    <div class="w-[80%] h-[2px] absolute left-0 rounded-full bg-gradient-to-r from-blue-800 to-[#191919]"></div>
+                    <div class="w-[2px] h-1/4 absolute left-0 bottom-[0%] rounded-b-full bg-gradient-to-t from-blue-800 to-[#191919] blue-enter-active opacity-0"></div>
                 </Transition>
                 <Transition name="red">
-                    <div class="w-[80%] h-[2px] absolute bottom-0 rounded-full bg-gradient-to-r from-red-800 to-[#191919]"></div>
+                    <div class="w-1/4 h-[2px] absolute bottom-0 right-[0%] rounded-full bg-gradient-to-r from-red-800 to-[#191919] red-enter-active opacity-0"></div>
                 </Transition>
                 <Transition name="pink">
-                    <div class="w-[80%] h-[90%] absolute border-r-2 rounded-xl border-r-pink-800"></div>
+                    <div class="w-[2px] h-1/4 absolute right-0 rounded-full bg-gradient-to-b from-pink-800 to-[#191919] pink-enter-active opacity-0"></div>
                 </Transition>
             </div>
         <div class="w-full sm:w-2/3 rounded-xl flex flex-col justify-between gap-4">
@@ -35,6 +35,52 @@
     </section>
 </template>
 
-<style scoped>
+<style>
+    @keyframes transitionTOP {
+        0% { left: 0; transform: translateX(0); opacity: 0.1; }
+        50% { opacity: 0.5; }
+        90% { opacity: 0.5; }
+        100% { left: 100%; transform: translateX(-100%); opacity: 0.1; } /* Stops at the container's end */
+    }
+    
+    .purple-enter-active {
+        animation: transitionTOP 5s ease-in-out forwards infinite;
+        animation-delay: 14s;
+    }
 
+    @keyframes transitionR {
+        0% { top: 0; transform: translateY(0); opacity: 0.1; }
+         50% { opacity: 0.5; }
+        90% { opacity: 0.5; }
+        100% { top: 100%; transform: translateY(-100%); } /* Stops at the container's end */
+    }
+    
+    .pink-enter-active {
+        animation: transitionR 5s ease-in-out forwards infinite;
+        animation-delay: 4s;
+    }
+
+    @keyframes transitionB {
+        0% { right: 0; transform: translateX(0); opacity: 0.1; }
+         50% { opacity: 0.5; }
+        90% { opacity: 0.5; }
+        100% { right: 100%; transform: translateX(100%); } /* Stops at the container's end */
+    }
+    
+    .red-enter-active {
+        animation: transitionB 5s ease-in-out forwards infinite;
+        animation-delay: 7s;
+    }
+
+    @keyframes transitionL {
+        0% { bottom: 0; transform: translateY(0); opacity: 0.1; }
+         50% { opacity: 0.5; }
+        90% { opacity: 0.5; }
+        100% { bottom: 100%; transform: translateY(100%); } /* Stops at the container's end */
+    }
+    
+    .blue-enter-active {
+        animation: transitionL 5s ease-in-out forwards infinite;
+        animation-delay: 11s;
+    }
 </style>
